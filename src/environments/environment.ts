3ed7025d2ee60c24c879ae8dfe81bd77fbeb90d5
DIMITRI EJARA <dimitri@ejara.lan>
2024-05-11 18:26:41 +0100

    ready for production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,8 @@
 export const environment = {
-  production: false,
+  production: true,
   apiUrl: 'http://app.good-data237',
   basePath: '/api/v0',
   publicPath: '/public',
   secretKey: '$9oOzfU6bQ5@',
   sessionTTL: 6000, // in minutes
 };
-
-
